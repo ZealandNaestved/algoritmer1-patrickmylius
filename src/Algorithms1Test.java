5acc970221be3efc77c5c2da80971ff721376061
patrickmylius <patrickmylius@outlook.dk>
2020-10-30 12:21:35 +0100

    Opgave 3 . Test af boyName w. notes

--- src/Algorithms1Test.java
+++ src/Algorithms1Test.java
@@ -6,9 +6,9 @@ class Algorithms1Test {
     //Testing random boy name
     @Test
     void randomBoyName() {
-        Algorithms1 testName = new Algorithms1();
+        Algorithms1 testName = new Algorithms1(); //Object of Algoritmhs1
 
-        testName.rand.setSeed(15L);
+        testName.rand.setSeed(15L); //Sets seed, to generate a "Specific" random numb. O.O
         testName.rand.nextInt(10);
 
         Assertions.assertEquals("Blær", testName.randomBoyName());
