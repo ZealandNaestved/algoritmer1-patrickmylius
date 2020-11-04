c5a058a9aee0b170e7f9feff3bc46f4f1455fb7b
patrickmylius <patrickmylius@outlook.dk>
2020-11-04 12:54:31 +0100

    Syntax

--- src/Algorithms2.java
+++ src/Algorithms2.java
@@ -7,18 +7,20 @@
 
 public class Algorithms2 {
     public static void main(String[] args) {
-        int []randomNumbers1000 = new Data().getRandomNumbers1000();
+        int[] randomNumbers1000 = new Data().getRandomNumbers1000();
         linearSearch(randomNumbers1000);
     }
+
     //Method searching for the value "77"
-    // TODO 1 Skriv en metode her i denne klasse, som kan finde et tal i Data klassens randomNumbers1000 felt med LINEAR SEARCH
+    //1 Skriv en metode her i denne klasse, som kan finde et tal i Data klassens randomNumbers1000 felt med LINEAR SEARCH
     public static void linearSearch(int[] randomNumbers1000) {
         int value = 77;
         //runs value agaisnt all numbs in randomNumbers1000 array
         for (int i = 0; i < randomNumbers1000.length; i++) {
             if (randomNumbers1000[i] == value) {
                 System.out.println("Number: " + value + " found in randomNumbers1000");
-            } else System.out.println(" ");;
+            } else
+                System.out.println(" ");
         }
     }
 
