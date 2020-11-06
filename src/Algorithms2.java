76c87eef2c200f608a9fe333eec0ab1c57427a62
patrickmylius <patrickmylius@outlook.dk>
2020-11-06 12:00:34 +0100

    Algorithms2 Exercise 3 solved.

--- src/Algorithms2.java
+++ src/Algorithms2.java
@@ -11,6 +11,13 @@ public class Algorithms2 {
         String[] randomBoyNames = new Data().getRandomDrengeNavne();
         linearSearch(randomNumbers1000);
         linearSearch(randomBoyNames);
+
+        int[] sortAnArray = selectionSort(randomNumbers1000);
+
+        for (int i = 0; i < sortAnArray.length; i++) {
+            System.out.println(sortAnArray[i]);
+        }
+
     }
 
     //Method linear searching for its expectedResult = 77
@@ -37,9 +44,35 @@ public class Algorithms2 {
 
         }
     }
-    // TODO 3 Skriv en metode her i denne klasse, som kan returnere et sorteret array fra randomNumbers1000
 
-    // TODO 3 Skriv en metode her i denne klasse, som kan returnere et sorteret array fra randomDrengeNavne
+    //3 Skriv en metode her i denne klasse, som kan returnere et sorteret array fra randomNumbers1000
+    //Method using selection sort to sort an array from low to high
+    //Returns the sorted array.
+    public static int[] selectionSort(int[] randomNumbers1000) {
+        int[] unSorted = new Data().getRandomNumbers1000();
+        int length = unSorted.length;
+
+        //Sets min value to 0
+        //search the minimum value in the array
+        for (int i = 0; i < length - 1; i++) {
+            int min = i;
+
+            //Swap with value at location MIN
+            for (int j = i + 1; j < length; j++) {
+                if (unSorted[j] < unSorted[min]) {
+                    min = j;
+                }
+            }
+            int temp = unSorted[min];
+            unSorted[min] = unSorted[i];
+            unSorted[i] = temp;
+        }
+        int[] sortedNumbs = unSorted;
+        return sortedNumbs;
+    }
+
+
+    // TODO 4 Skriv en metode her i denne klasse, som kan returnere et sorteret array fra randomDrengeNavne
 
     // TODO 5 Skriv en metode her i denne klasse, som kan finde et navn i Data klassens randomNumbers1000 felt med BINARY SEARCH
 
